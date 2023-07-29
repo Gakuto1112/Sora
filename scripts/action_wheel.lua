f2c8709fd2300d143b0c34fa07ea67d0373c1d9a
Gakuto1112 <gakuto131112@gmail.com>
2023-07-29 14:10:18 +0900

    表示名変更機能の追加

--- scripts/action_wheel.lua
+++ scripts/action_wheel.lua
@@ -10,17 +10,45 @@ ActionWheel = {
 		if host:isHost() then
 			ActionWheel.MainPage:getAction(1):toggled(false)
 		end
-	end,
-
+	end
 }
 
----ping関数
+---前チックにアクションホイールを開けていたかどうか
+---@type boolean
+local isOpenActionWheelPrev = false
+
+---プレイヤーの表示名の状態を示す：1. プレイヤー名, 2. Sora, 3. 夜空
+---@type string
+local playerNameState = Config.loadConfig("name", 1)
+
+---プレイヤーの現在の表示名の状態を示す：1. プレイヤー名, 2. Sora, 3. 夜空
+---@type string
+local currentPlayerNameState = playerNameState
+
+---名前変更のアクションの名称を変更する。
+local function setNameChangeActionTitle()
+    if playerNameState == currentPlayerNameState then
+        ActionWheel.MainPage:getAction(2):title(Language.getTranslate("action_wheel__main__action_2__title").."§b"..Nameplate.NameList[playerNameState])
+    else
+        ActionWheel.MainPage:getAction(2):title(Language.getTranslate("action_wheel__main__action_2__title").."§b"..Nameplate.NameList[playerNameState].."\n§7"..Language.getTranslate("action_wheel__close_to_confirm"))
+    end
+end
+
+--ping関数
 function pings.main_action1_toggle()
 	SitDown:play()
 end
 
 function pings.main_action1_untoggle()
 	SitDown:stop()
+end
+
+function pings.main_action2(nameID)
+	nameplate.ALL:setText(Nameplate.NameList[nameID])
+	currentPlayerNameState = nameID
+	if host:isHost() then
+		setNameChangeActionTitle()
+	end
 end
 
 events.TICK:register(function ()
@@ -33,6 +61,15 @@ events.TICK:register(function ()
                 ActionWheel.MainPage:getAction(1):title("§7"..Language.getTranslate("action_wheel__main__action_1__title")):color(0.16, 0.16, 0.16):hoverColor(1, 0.33, 0.33)
             end
         end
+        if not isOpenActionWheel and isOpenActionWheelPrev then
+            if playerNameState ~= currentPlayerNameState then
+                pings.main_action2(playerNameState)
+                Config.saveConfig("name", playerNameState)
+				sounds:playSound("minecraft:ui.cartography_table.take_result", player:getPos(), 1, 1)
+				print(Language.getTranslate("action_wheel__main__action_2__done_first")..Nameplate.NameList[playerNameState]..Language.getTranslate("action_wheel__main__action_2__done_last"))
+            end
+        end
+        isOpenActionWheelPrev = isOpenActionWheel
     end
 end)
 
@@ -55,7 +92,20 @@ if host:isHost() then
     end)
 
     --アクション1-2. 名前変更
-	ActionWheel.MainPage:newAction(2):title(Language.getTranslate("action_wheel__main__action_2__title")):color(0.91, 0.67, 0.27):hoverColor(1, 1, 1):item("name_tag"):color(0.78, 0.78, 0.78):hoverColor(1, 1, 1)
+	ActionWheel.MainPage:newAction(2):title(Language.getTranslate("action_wheel__main__action_2__title")):color(0.91, 0.67, 0.27):hoverColor(1, 1, 1):item("name_tag"):color(0.78, 0.78, 0.78):hoverColor(1, 1, 1):onScroll(function (direction)
+        if direction < 0 then
+            playerNameState = playerNameState == #Nameplate.NameList and 1 or playerNameState + 1
+        else
+            playerNameState = playerNameState == 1 and #Nameplate.NameList or playerNameState - 1
+        end
+        setNameChangeActionTitle()
+    end):onLeftClick(function ()
+        playerNameState = currentPlayerNameState
+        setNameChangeActionTitle()
+    end):onRightClick(function ()
+        playerNameState = 1
+        setNameChangeActionTitle()
+    end)
 
     --アクション1-3. 防具の表示
 	ActionWheel.MainPage:newAction(3):title(Language.getTranslate("action_wheel__main__action_3__title")..Language.getTranslate("action_wheel__toggle_off")):toggleTitle(Language.getTranslate("action_wheel__main__action_3__title")..Language.getTranslate("action_wheel__toggle_on")):item("iron_chestplate"):color(0.67, 0, 0):hoverColor(1, 0.33, 0.33)
@@ -72,6 +122,7 @@ if host:isHost() then
     --アクション1-7. 頻出メッセージの表示
 	ActionWheel.MainPage:newAction(7):title(Language.getTranslate("action_wheel__main__action_7__title")..Language.getTranslate("action_wheel__toggle_off")):toggleTitle(Language.getTranslate("action_wheel__main__action_7__title")..Language.getTranslate("action_wheel__toggle_on")):item("cake"):color(0.67, 0, 0):hoverColor(1, 0.33, 0.33):toggleColor(0, 0.67, 0)
 
+    setNameChangeActionTitle()
     action_wheel:setPage(ActionWheel.MainPage)
 end
 
