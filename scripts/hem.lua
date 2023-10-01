ddc8b872b135b761bceeccc6565539041a41aca1
Gakuto1112 <gakuto131112@gmail.com>
2023-10-01 17:44:36 +0900

    一人称視点で影/ペーパードールの袖が動くように変更

--- scripts/hem.lua
+++ scripts/hem.lua
@@ -6,16 +6,17 @@ Hem = {
 	---エプロンを有効にする。
 	enable = function ()
 		models.models.main.Avatar.UpperBody.Body.Hem:setVisible(true)
-		events.RENDER:remove("hem_render")
-		events.RENDER:register(function ()
-			if player:isCrouching() then
-				models.models.main.Avatar.UpperBody.Body.Hem:setRot(vectors.vec3(math.min(vanilla_model.RIGHT_LEG:getOriginRot().x, vanilla_model.LEFT_LEG:getOriginRot().x) + 30) + Hem.RotOffset)
-				models.models.main.Avatar.UpperBody.Body.Hem:offsetPivot(0, -1, 1)
-			else
-				models.models.main.Avatar.UpperBody.Body.Hem:setRot(vectors.vec3(math.min(vanilla_model.RIGHT_LEG:getOriginRot().x, vanilla_model.LEFT_LEG:getOriginRot().x)) + Hem.RotOffset)
-				models.models.main.Avatar.UpperBody.Body.Hem:offsetPivot()
-			end
-		end, "hem_render")
+		if events.RENDER:getRegisteredCount("hem_render") == 0 then
+			events.RENDER:register(function ()
+				if player:isCrouching() then
+					models.models.main.Avatar.UpperBody.Body.Hem:setRot(vectors.vec3(math.min(vanilla_model.RIGHT_LEG:getOriginRot().x, vanilla_model.LEFT_LEG:getOriginRot().x) + 30) + Hem.RotOffset)
+					models.models.main.Avatar.UpperBody.Body.Hem:offsetPivot(0, -1, 1)
+				else
+					models.models.main.Avatar.UpperBody.Body.Hem:setRot(vectors.vec3(math.min(vanilla_model.RIGHT_LEG:getOriginRot().x, vanilla_model.LEFT_LEG:getOriginRot().x)) + Hem.RotOffset)
+					models.models.main.Avatar.UpperBody.Body.Hem:offsetPivot()
+				end
+			end, "hem_render")
+		end
 	end,
 
 	---裾を無効にする。
